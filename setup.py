08c3353be6c3ba1c33ca1ff7878f6fc061b040a0
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-01-15 16:26:04 -0500

    adde numpy scipy and cython to setup requires

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
+SETUP_REQUIRES = ['numpy>=1.6.1', 'scipy>=0.9', 'cython>=0.23']
+
 setup(name='scikit-learn stub',
       version='0.1.0_dev',
       description='A template for scikit-learn compatible packages',
       author='Vighnesh Birodkar',
       packages=find_packages(),
+      setup_requires=SETUP_REQUIRES,
       install_requires=INSTALL_REQUIRES,
       author_email='vighneshbirodkar@nyu.edu',
       )
