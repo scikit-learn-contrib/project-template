d4176d506b3fa5d56cb1423f2b80bf91507e223d
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-01-15 16:52:53 -0500

    remove setup requires option

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import sys
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
@@ -9,19 +10,19 @@ try:
     import numpy
 except ImportError:
     print('numpy is required during installation')
+    sys.exit(1)
 
 try:
     import scipy
 except ImportError:
     print('scipy is required during installation')
-
+    sys.exit(1)
 
 setup(name='scikit-learn stub',
       version='0.1.0_dev',
       description='A template for scikit-learn compatible packages',
       author='Vighnesh Birodkar',
       packages=find_packages(),
-      setup_requires=SETUP_REQUIRES,
       install_requires=INSTALL_REQUIRES,
       author_email='vighneshbirodkar@nyu.edu',
       )
