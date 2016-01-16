2e798900757ebd9e8ffb00e2c519dcecfbf8c7aa
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-01-15 19:42:46 -0500

    no timer in tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ mkdir -p $TEST_DIR
 cd $TEST_DIR
 
 if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage --with-timer --timer-top-n 20 sklstub
+    nosetests -s --with-coverage sklstub
 else
-    nosetests -s --with-timer --timer-top-n 20 sklstub
+    nosetests -s sklstub
 fi
