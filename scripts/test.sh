06b21c6f1e5a5763169713d46b8d345ea8dafde3
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-01-15 19:58:25 -0500

    create success script

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ mkdir -p $TEST_DIR
 cd $TEST_DIR
 
 if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage sklstub
+    nosetests -s --with-coverage --cover-package=sklstub sklstub
 else
     nosetests -s sklstub
 fi
