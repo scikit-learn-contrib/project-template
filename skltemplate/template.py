4a6beff12ed777fb23153775c18e47220784b392
omtinez <omtinez@gmail.com>
2018-03-24 18:45:07 -0700

    Fix AssertionError when using TemplateTransformer
    As-is, TemplateTransformer fails with the following code:
    
        from sklearn.utils.estimator_checks import check_estimator
        tpl = TemplateTransformer()
        check_estimator(tpl)
    
    > AssertionError: Error message does not include the expected string: 'Reshape your data'. Observed error message: 'Shape of input is different from what was seen in `fit`'
    
    Changing the message within ValueError to include the expected string fixes this issue.

--- skltemplate/template.py
+++ skltemplate/template.py
@@ -185,6 +185,6 @@ class TemplateTransformer(BaseEstimator, TransformerMixin):
         # Check that the input is of the same shape as the one passed
         # during fit.
         if X.shape != self.input_shape_:
-            raise ValueError('Shape of input is different from what was seen'
-                             'in `fit`')
+            raise ValueError('Reshape your data. Shape of input is different'
+                             ' from what was seen in `fit`')
         return np.sqrt(X)
