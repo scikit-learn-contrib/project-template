42a4175efe0b645856fb5a433e07de6631f5fc68
Romain Brault <romain.brault@protonmail.com>
2018-03-30 17:44:15 +0200

    forgot module

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -7,7 +7,7 @@ mkdir -p $TEST_DIR
 cd $TEST_DIR
 
 if [[ "$COVERAGE" == "true" ]]; then
-    py.test -v -x --doctest-modules --ignore=setup.py --cov=$MODULE
+    py.test -v -x --doctest-modules --ignore=setup.py --cov=$MODULE $MODULE
 else
-    py.test -v -x --doctest-modules --ignore=setup.py
+    py.test -v -x --doctest-modules --ignore=setup.py $MODULE
 fi
