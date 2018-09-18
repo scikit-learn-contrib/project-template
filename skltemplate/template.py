93d59bfee833c3d04267c091781c0bdcd7314ce1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-09-19 00:58:41 +0200

    TST: improve and cover all code

--- skltemplate/template.py
+++ skltemplate/template.py
@@ -13,20 +13,20 @@ class TemplateEstimator(BaseEstimator):
 
     Parameters
     ----------
-    demo_param : str, optional
+    demo_param : str, default='demo_param'
         A parameter used for demonstation of how to pass and store paramters.
     """
     def __init__(self, demo_param='demo_param'):
         self.demo_param = demo_param
 
     def fit(self, X, y):
-        """A reference implementation of a fitting function
+        """A reference implementation of a fitting function.
 
         Parameters
         ----------
-        X : array-like or sparse matrix of shape = [n_samples, n_features]
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
             The training input samples.
-        y : array-like, shape = [n_samples] or [n_samples, n_outputs]
+        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
             The target values (class labels in classification, real numbers in
             regression).
 
@@ -35,8 +35,9 @@ class TemplateEstimator(BaseEstimator):
         self : object
             Returns self.
         """
-        X, y = check_X_y(X, y)
-        # Return the estimator
+        X, y = check_X_y(X, y, accept_sparse=True)
+        self.is_fitted_ = True
+        # `fit` should always return `self`
         return self
 
     def predict(self, X):
@@ -44,16 +45,17 @@ class TemplateEstimator(BaseEstimator):
 
         Parameters
         ----------
-        X : array-like of shape = [n_samples, n_features]
-            The input samples.
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
 
         Returns
         -------
-        y : array of shape = [n_samples]
-            Returns :math:`x^2` where :math:`x` is the first column of `X`.
+        y : ndarray, shape (n_samples,)
+            Returns an array of ones.
         """
-        X = check_array(X)
-        return X[:, 0]**2
+        X = check_array(X, accept_sparse=True)
+        check_is_fitted(self, 'is_fitted_')
+        return np.ones(X.shape[0], dtype=np.int64)
 
 
 class TemplateClassifier(BaseEstimator, ClassifierMixin):
@@ -61,15 +63,17 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
 
     Parameters
     ----------
-    demo_param : str, optional
+    demo_param : str, default='demo'
         A parameter used for demonstation of how to pass and store paramters.
 
     Attributes
     ----------
-    X_ : array, shape = [n_samples, n_features]
-        The input passed during :meth:`fit`
-    y_ : array, shape = [n_samples]
-        The labels passed during :meth:`fit`
+    X_ : ndarray, shape (n_samples, n_features)
+        The input passed during :meth:`fit`.
+    y_ : ndarray, shape (n_samples,)
+        The labels passed during :meth:`fit`.
+    classes_ : ndarray, shape (n_classes,)
+        The classes seen at :meth:`fit`.
     """
     def __init__(self, demo_param='demo'):
         self.demo_param = demo_param
@@ -79,9 +83,9 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
 
         Parameters
         ----------
-        X : array-like, shape = [n_samples, n_features]
+        X : array-like, shape (n_samples, n_features)
             The training input samples.
-        y : array-like, shape = [n_samples]
+        y : array-like, shape (n_samples,)
             The target values. An array of int.
 
         Returns
@@ -104,12 +108,12 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
 
         Parameters
         ----------
-        X : array-like of shape = [n_samples, n_features]
+        X : array-like, shape (n_samples, n_features)
             The input samples.
 
         Returns
         -------
-        y : array of int of shape = [n_samples]
+        y : ndarray, shape (n_samples,)
             The label for each sample is the label of the closest sample
             seen udring fit.
         """
@@ -128,13 +132,13 @@ class TemplateTransformer(BaseEstimator, TransformerMixin):
 
     Parameters
     ----------
-    demo_param : str, optional
+    demo_param : str, default='demo'
         A parameter used for demonstation of how to pass and store paramters.
 
     Attributes
     ----------
-    input_shape : tuple
-        The shape the data passed to :meth:`fit`
+    n_features_ : int
+        The number of features of the data passed to :meth:`fit`.
     """
     def __init__(self, demo_param='demo'):
         self.demo_param = demo_param
@@ -144,7 +148,7 @@ class TemplateTransformer(BaseEstimator, TransformerMixin):
 
         Parameters
         ----------
-        X : array-like or sparse matrix of shape = [n_samples, n_features]
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
             The training input samples.
         y : None
             There is no need of a target in a transformer, yet the pipeline API
@@ -155,9 +159,9 @@ class TemplateTransformer(BaseEstimator, TransformerMixin):
         self : object
             Returns self.
         """
-        X = check_array(X)
+        X = check_array(X, accept_sparse=True)
 
-        self.input_shape_ = X.shape
+        self.n_features_ = X.shape[1]
 
         # Return the transformer
         return self
@@ -167,24 +171,24 @@ class TemplateTransformer(BaseEstimator, TransformerMixin):
 
         Parameters
         ----------
-        X : array-like of shape = [n_samples, n_features]
+        X : {array-like, sparse-matrix}, shape (n_samples, n_features)
             The input samples.
 
         Returns
         -------
-        X_transformed : array of int of shape = [n_samples, n_features]
+        X_transformed : array, shape (n_samples, n_features)
             The array containing the element-wise square roots of the values
-            in `X`
+            in ``X``.
         """
         # Check is fit had been called
-        check_is_fitted(self, ['input_shape_'])
+        check_is_fitted(self, 'n_features_')
 
         # Input validation
-        X = check_array(X)
+        X = check_array(X, accept_sparse=True)
 
         # Check that the input is of the same shape as the one passed
         # during fit.
-        if X.shape != self.input_shape_:
+        if X.shape[1] != self.n_features_:
             raise ValueError('Shape of input is different from what was seen'
                              'in `fit`')
         return np.sqrt(X)
