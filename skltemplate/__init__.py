ab32df65343e9e6ba916e50831382d4745273b3a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-09-19 00:01:49 +0200

    MAINT: udpate setup.py

--- skltemplate/__init__.py
+++ skltemplate/__init__.py
@@ -1,6 +1,8 @@
 from .template import (TemplateEstimator, TemplateClassifier,
                        TemplateTransformer)
 from . import template
+from ._version import __version__
 
 __all__ = ['TemplateEstimator', 'TemplateClassifier',
-           'TemplateTransformer', 'template']
+           'TemplateTransformer', 'template',
+           '__version__']
