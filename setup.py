6e107d547ef47d6ca855a400b81c996840c69668
Leo Chow <57784789+leockl@users.noreply.github.com>
2020-01-21 14:33:42 +1100

    Create setup.py file

--- setup.py
+++ setup.py
@@ -1,65 +1,23 @@
-#! /usr/bin/env python
-"""A template for scikit-learn compatible packages."""
+import setuptools
 
-import codecs
-import os
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-from setuptools import find_packages, setup
-
-# get __version__ from _version.py
-ver_file = os.path.join('skltemplate', '_version.py')
-with open(ver_file) as f:
-    exec(f.read())
-
-DISTNAME = 'sklearn-template'
-DESCRIPTION = 'A template for scikit-learn compatible packages.'
-with codecs.open('README.rst', encoding='utf-8-sig') as f:
-    LONG_DESCRIPTION = f.read()
-MAINTAINER = 'V. Birodkars, G. Lemaitre'
-MAINTAINER_EMAIL = 'vighneshbirodkar@nyu.edu, g.lemaitre58@gmail.com'
-URL = 'https://github.com/scikit-learn-contrib/project-template'
-LICENSE = 'new BSD'
-DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/project-template'
-VERSION = __version__
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved',
-               'Programming Language :: Python',
-               'Topic :: Software Development',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: Microsoft :: Windows',
-               'Operating System :: POSIX',
-               'Operating System :: Unix',
-               'Operating System :: MacOS',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
-EXTRAS_REQUIRE = {
-    'tests': [
-        'pytest',
-        'pytest-cov'],
-    'docs': [
-        'sphinx',
-        'sphinx-gallery',
-        'sphinx_rtd_theme',
-        'numpydoc',
-        'matplotlib'
-    ]
-}
-
-setup(name=DISTNAME,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      long_description=LONG_DESCRIPTION,
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+setuptools.setup(
+    name="hqc", # Replace with your own username
+    version="0.0.3",
+    author="Leo Chow",
+    author_email="leo_ckl@hotmail.com",
+    description="A package for the Helstrom Quantum Centroid classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leockl/helstrom-quantum-centroid-classifier",
+    keyword=["machine learning", "classification", "classifier", "Helstrom", "helstrom", "Quantum", "quantum", "Quantum-inspired", "quantum-inspired"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
