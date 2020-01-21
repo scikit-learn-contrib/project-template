04c5ea23f7684474cb4e0ba764fb0de1bedfb483
Leo Chow <57784789+leockl@users.noreply.github.com>
2020-01-21 15:01:25 +1100

    Create setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hqc", # Replace with your own username
+    name="HQC", # Replace with your own username
     version="0.0.3",
     author="Leo Chow",
     author_email="leo_ckl@hotmail.com",
