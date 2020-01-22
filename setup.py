46b430c018a515d19b25618631f25b079733af9a
Leo Chow <57784789+leockl@users.noreply.github.com>
2020-01-22 16:32:08 +1100

    Create setup.py file

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hqc", # Replace with your own username
-    version="0.0.3",
+    name="HQC", # Replace with your own username
+    version="0.1.0",
     author="Leo Chow",
     author_email="leo_ckl@hotmail.com",
-    description="A package for the Helstrom Quantum Centroid classifier",
+    description="A package for the Helstrom Quantum Centroid Classifier",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/leockl/helstrom-quantum-centroid-classifier",
