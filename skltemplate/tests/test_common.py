bf9e883906b5dd9c02adfa2dc523222a5db6d5ce
radon <remi.adon@gmail.com>
2020-05-19 12:07:48 +0200

    test_common.py makes use of list_all_estimators

--- skltemplate/tests/test_common.py
+++ skltemplate/tests/test_common.py
@@ -1,14 +1,10 @@
 import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
-
-from skltemplate import TemplateEstimator
-from skltemplate import TemplateClassifier
-from skltemplate import TemplateTransformer
-
+from .._list_estimators import list_all_estimators
 
 @pytest.mark.parametrize(
-    "Estimator", [TemplateEstimator, TemplateTransformer, TemplateClassifier]
+    "EstimatorName, Estimator", list_all_estimators()
 )
-def test_all_estimators(Estimator):
+def test_all_estimators(EstimatorName, Estimator):
     return check_estimator(Estimator)
