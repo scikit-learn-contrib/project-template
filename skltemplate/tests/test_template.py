c88a56e73f8182f9d5c915340fa90e622eca0d69
Wenjie Zheng <work@zhengwenjie.net>
2021-06-03 23:04:11 +0800

    MAINT Import testing tools from numpy instead (#67)
    Aim for the compatibility with all scikit-learn versions

--- skltemplate/tests/test_template.py
+++ skltemplate/tests/test_template.py
@@ -2,8 +2,8 @@ import pytest
 import numpy as np
 
 from sklearn.datasets import load_iris
-from sklearn.utils.testing import assert_array_equal
-from sklearn.utils.testing import assert_allclose
+from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose
 
 from skltemplate import TemplateEstimator
 from skltemplate import TemplateTransformer
