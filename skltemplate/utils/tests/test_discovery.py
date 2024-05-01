0bfd40cacc7ab75883dd1742207ad48b8e153c07
Guillaume Lemaitre <guillaume@probabl.ai>
2024-05-01 18:58:58 +0200

    TST add more test for discovery module

--- skltemplate/utils/tests/test_discovery.py
+++ skltemplate/utils/tests/test_discovery.py
@@ -1,12 +1,24 @@
 # Authors: scikit-learn-contrib developers
 # License: BSD 3 clause
 
+import pytest
+
 from skltemplate.utils.discovery import all_displays, all_estimators, all_functions
 
 
 def test_all_estimators():
     estimators = all_estimators()
     assert len(estimators) == 3
+
+    estimators = all_estimators(type_filter="classifier")
+    assert len(estimators) == 1
+
+    estimators = all_estimators(type_filter=["classifier", "transformer"])
+    assert len(estimators) == 2
+
+    err_msg = "Parameter type_filter must be"
+    with pytest.raises(ValueError, match=err_msg):
+        all_estimators(type_filter="xxxx")
 
 
 def test_all_displays():
